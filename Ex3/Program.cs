c064ef6cb56e0c1e9a0453174682c6a149c53209
Saglara <nohaevass@yandex.ru>
2022-10-16 14:05:51 +0300

    Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр с помощью рекурсии.

---
+++ Ex3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число 
+//и возвращать сумму его цифр с помощью рекурсии.
+// 453 -> 12
+// 45 -> 9
+
+Console.Clear();
+Console.Write("Введите число n = ");
+int num =  Convert.ToInt32(Console.ReadLine());
+
+int result = SumOfDigit(num);
+Console.WriteLine("Сумма цифр в " + num + " равна " + result);
+
+int SumOfDigit(int n)
+{
+    if (n == 0)
+        return 0;
+
+    return (n % 10 + SumOfDigit(n / 10));
+}
